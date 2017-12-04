5f4cc8f6aa26f42bec7597eceda8d2408de231d6
Jochen Mader <pflanzenmoerder@gmail.com>
2017-12-05 00:19:13 +0100

    Ugly but working top, needs some removal of duplicated code

--- src/display_elements.rs
+++ src/display_elements.rs
@@ -3,31 +3,50 @@ pub mod display_elements {
 
     extern crate termion;
     use std::io::{Write};
-    fn draw_bar<W: Write>(stdout: &mut W, used:u64) {
+    fn draw_bar<W: Write>(stdout: &mut W, free:f32, width:u16) {
 
-        for _ in 0..used {
-            write!(stdout,
-                   "{} ",
-                   termion::color::Bg(termion::color::Rgb(255, 0, 0)))
-                .unwrap();
+        let taken: u16 = 21; //amount of space used by static content, should be more dynamic ...
+
+        write!(stdout,
+               "{}[ ",
+               termion::color::Fg(termion::color::Rgb(255, 255, 255)))
+            .unwrap();
+
+        if width > taken {
+            let scale = (width - taken) as u32;
+
+            let used:u32 = scale - (free / 100.0 * scale as f32) as u32;
+
+            for _ in 0..used {
+                write!(stdout,
+                       "{}|",
+                       termion::color::Fg(termion::color::Rgb(255, 0, 0)))
+                    .unwrap();
+            }
+
+            for _ in used..scale {
+                write!(stdout,
+                       "{}|",
+                       termion::color::Fg(termion::color::Rgb(0, 255, 0)))
+                    .unwrap();
+            }
         }
 
-        for _ in used..100 {
-            write!(stdout,
-                   "{} ",
-                   termion::color::Bg(termion::color::Rgb(0, 255, 0)))
-                .unwrap();
-        }
+        write!(stdout,
+               "{} {:.2} % ]",
+               termion::color::Fg(termion::color::Rgb(255, 255, 255)),
+               100.0 - free )
+            .unwrap();
     }
 
     pub struct LabelledBar {
-        used: Option<u64>,
-        used_fn: Box<Fn() -> u64>,
+        used: Option<f32>,
+        used_fn: Box<Fn() -> f32>,
         label: String
     }
 
     impl LabelledBar {
-        pub fn new(label: String, used_fn: Box<Fn() -> u64>) -> LabelledBar {
+        pub fn new(label: String, used_fn: Box<Fn() -> f32>) -> LabelledBar {
             LabelledBar {
                 label: label,
                 used: None,
@@ -35,13 +54,12 @@ pub mod display_elements {
             }
         }
 
-
-        pub fn update<W: Write>(&mut self, stdout: &mut W) {
+        pub fn update<W: Write>(&mut self, stdout: &mut W, width:u16) {
             let new_used = Some((self.used_fn)());
             if self.used != new_used {
                 self.used = new_used;
                 write!(stdout, "{}{}", termion::style::Reset, self.label).unwrap();
-                draw_bar(stdout, self.used.unwrap());
+                draw_bar(stdout, self.used.unwrap(), width);
                 write!(stdout, "\n\r").unwrap();
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -14,35 +14,71 @@ use display_elements::display_elements::*;
 
 fn memory_bar(mem_info:MemInfo) -> LabelledBar {
     let one_slot = mem_info.total / 100;
-    let clr: Box<Fn() -> u64> = Box::new(move || {
-        let nr_slots = mem_info.free / one_slot;
-        100 - nr_slots
-    });
-    LabelledBar::new("memory (used/free): ".to_string(), clr)
+    let clr: Box<Fn() -> f32> = Box::new(move || mem_info.avail as f32 / one_slot as f32);
+    LabelledBar::new("memory : ".to_string(), clr)
 }
 
 fn swap_bar(swap_info:SwapInfo) -> LabelledBar {
     let one_slot = swap_info.total / 100;
-    let clr: Box<Fn() -> u64> = Box::new(move || {
-        let nr_slots = swap_info.free / one_slot;
-        100 - nr_slots
-    });
-    LabelledBar::new("swap (used/free)  : ".to_string(), clr)
+    let clr: Box<Fn() -> f32> = Box::new(move || swap_info.free as f32 / one_slot as f32);
+    LabelledBar::new("swap   : ".to_string(), clr)
 }
 
 fn disk_bar(disk_info:DiskInfo) -> LabelledBar {
     let one_slot = disk_info.total / 100;
-    let clr: Box<Fn() -> u64> = Box::new(move || {
-        let nr_slots = disk_info.free / one_slot;
-        100 - nr_slots
-    });
-    LabelledBar::new("disk (used/free)  : ".to_string(), clr)
+    let clr: Box<Fn() -> f32> = Box::new(move || disk_info.free as f32 / one_slot as f32);
+    LabelledBar::new("disk   : ".to_string(), clr)
+}
+
+//fn cpu_bar(cpu_info:CpuInfo) -> LabelledBar {
+//    let one_slot = cpu_info.total / 100;
+//    let clr: Box<Fn() -> u64> = Box::new(move || {
+//        let nr_slots = disk_info.free / one_slot;
+//        100 - nr_slots
+//    });
+//    LabelledBar::new("disk (used/free)  : ".to_string(), clr)
+//}
+
+fn print_cpu<W: Write>(stdout: &mut W, cpu_info:CpuInfo, load_info:LoadInfo) {
+
+    let one_color =
+        if load_info.one > cpu_info.num as f64 {termion::color::Rgb(255, 0, 0)}
+        else if load_info.one >= (cpu_info.num as f64 * 0.8){termion::color::Rgb(255, 255, 0)}
+        else {termion::color::Rgb(0, 255, 0)};
+
+    let five_color =
+        if load_info.five > cpu_info.num as f64 {termion::color::Rgb(255, 0, 0)}
+        else if load_info.five >= (cpu_info.num as f64 * 0.8){termion::color::Rgb(255, 255, 0)}
+        else {termion::color::Rgb(0, 255, 0)};
+
+    let fiveteen_color =
+        if load_info.fiveteen > cpu_info.num as f64 {termion::color::Rgb(255, 0, 0)}
+        else if load_info.fiveteen >= (cpu_info.num as f64 * 0.8){termion::color::Rgb(255, 255, 0)}
+        else {termion::color::Rgb(0, 255, 0)};
+
+
+    write!(stdout,
+            "{}[ #CPU: {} ][ spd: {} ][ 1: {}{:.2}{} 5: {}{:.2}{} 15: {}{:.2}{} ]",
+            termion::color::Fg(termion::color::Rgb(255, 255, 255)),
+            cpu_info.num,
+            cpu_info.speed,
+            termion::color::Fg(one_color),
+            load_info.one,
+            termion::color::Fg(termion::color::Rgb(255, 255, 255)),
+            termion::color::Fg(five_color),
+            load_info.five,
+            termion::color::Fg(termion::color::Rgb(255, 255, 255)),
+            termion::color::Fg(fiveteen_color),
+            load_info.fiveteen,
+            termion::color::Fg(termion::color::Rgb(255, 255, 255)))
+        .unwrap();
+    write!(stdout, "\n\r").unwrap();
 }
 
 fn main() {
     let mut stdout = stdout().into_raw_mode().unwrap();
 
-    writeln!(stdout,
+    write!(stdout,
              "{}{}{}Use the up/down arrow keys to change the blue in the rainbow.",
              termion::clear::All,
              termion::cursor::Goto(1, 1),
@@ -62,11 +98,14 @@ fn main() {
                termion::clear::All)
             .unwrap();
         let sysinfo = SysInfo::new();
-        writeln!(stdout, "{}", sysinfo.swap_info.total);
-        memory_bar(sysinfo.mem_info).update(&mut stdout);
-        disk_bar(sysinfo.disk_info).update(&mut stdout);
+        let (width,height) = termion::terminal_size().unwrap();
+
+        print_cpu(&mut stdout, sysinfo.cpu_info, sysinfo.load_info);
+        memory_bar(sysinfo.mem_info).update(&mut stdout, width);
+        disk_bar(sysinfo.disk_info).update(&mut stdout, width);
+//        cpu_bar(sysinfo.cpu_info).update(&mut stdout);
         write!(stdout, "{} ", termion::style::Reset).unwrap();
-        thread::sleep(Duration::from_millis(500));
+        thread::sleep(Duration::from_millis(100));
     }
     println!("Got it! Exiting...");
 
